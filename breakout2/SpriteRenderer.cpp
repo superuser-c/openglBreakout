42e11c87f3ea0006ba13c87cbb5384cac7645de9
superuser-c <62846333+superuser-c@users.noreply.github.com>
2020-03-29 20:50:27 +0200

    Update SpriteRenderer.cpp

--- breakout2/SpriteRenderer.cpp
+++ breakout2/SpriteRenderer.cpp
@@ -17,7 +17,7 @@ void SpriteRenderer::DrawSprite(Texture2D &texture, glm::vec2 position, glm::vec
 {
 	// Prepare transformations
 	this->shader.Use();
-	glm::mat4 model;
+	glm::mat4 model(1.0f);
 	model = glm::translate(model, glm::vec3(position, 0.0f));  // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
 
 	model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f)); // Move origin of rotation to center of quad
